8e90765ffbe6c7c95d247b7796e86db8da9099ba
baddou-rachid <rachidbaddou2000@gmail.com>
2022-10-20 21:54:52 +0100

    Change in the while boucle

--- sujetB.py
+++ sujetB.py
@@ -6,7 +6,7 @@ m=n
 s=0
 i=0
 q=n
-while q != 0 :
+while True :
     i=i+1
     q = n//10
     r = n % 10
@@ -14,6 +14,8 @@ while q != 0 :
         l=r
     s = s*10+r
     n = q
+    if q==0:
+        break
 if s == m and s == m*l :
     print(f"{m} est un entier propre et symetrique")
 elif s==m :
